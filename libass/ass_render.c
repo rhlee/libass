1710fecd264c2069aa44bda7347ac22fca3c3807
Richard H Lee <ricardohenrylee@gmail.com>
2012-08-21 23:42:27 +0100

    remove y offset

--- libass/ass_render.c
+++ libass/ass_render.c
@@ -1021,7 +1021,6 @@ static void draw_opaque_box(ASS_Renderer *render_priv, int asc, int desc,
     int i;
     int adv = advance.x;
     // The yoffset helps center the text
-    int yoffset = desc / 6;
     double scale_y = render_priv->state.scale_y;
     double scale_x = render_priv->state.scale_x;
 
@@ -1042,10 +1041,10 @@ static void draw_opaque_box(ASS_Renderer *render_priv, int asc, int desc,
     desc += asc * (scale_y - 1.0);
 
     FT_Vector points[4] = {
-        { .x = -sx,         .y = asc + sy - yoffset},
-        { .x = adv + sx,    .y = asc + sy - yoffset},
-        { .x = adv + sx,    .y = -desc - sy - yoffset},
-        { .x = -sx,         .y = -desc - sy - yoffset},
+        { .x = -sx,         .y = asc + sy},
+        { .x = adv + sx,    .y = asc + sy},
+        { .x = adv + sx,    .y = -desc - sy},
+        { .x = -sx,         .y = -desc - sy},
     };
 
     FT_Outline_New(render_priv->ftlibrary, 4, 1, ol);
